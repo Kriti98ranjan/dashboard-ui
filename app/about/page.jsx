8939ae39c16155a0cd92c6e76deeab029fef0ece
Kriti98ranjan <kritiranjan196@gmail.com>
2026-07-02 17:35:29 +0530

    This is About page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,12 +10,16 @@ export default function AboutPage() {
         <Link
           href="/dashboard"
           className="text-sm bg-blue-600 text-white px-4 py-1.5 rounded"
-        >Dashboard
+        >
+          Dashboard
         </Link>
       </nav>
 
       <div className="max-w-2xl mx-auto px-6 py-16">
-        <Link href="/" className="text-sm bg-amber-400 px-6 py-2 rounded text-blue-600 hover:bg-white mb-6 inline-block">
+        <Link
+          href="/"
+          className="text-sm bg-amber-400 px-6 py-2 rounded text-blue-600 hover:bg-white mb-6 inline-block"
+        >
           Back to Home
         </Link>
 
@@ -25,7 +29,8 @@ export default function AboutPage() {
 
             <div className="bg-white rounded-lg shadow p-6">
               <p className="text-gray-700 mb-4">
-                This dashboard is built using <strong>Next.js</strong>, and <strong>Tailwind CSS</strong>.
+                This dashboard is built using <strong>Next.js</strong>, and{" "}
+                <strong>Tailwind CSS</strong>.
               </p>
 
               <h2 className="text-xl font-semibold mb-2">Features</h2>
@@ -50,7 +55,9 @@ export default function AboutPage() {
 
         <Link
           href="/dashboard"
-          className="inline-block mt-8 bg-blue-600 text-white px-6 py-2.5  hover:bg-amber-600 rounded-lg">Open Dashboard
+          className="inline-block mt-8 bg-blue-600 text-white px-6 py-2.5  hover:bg-amber-600 rounded-lg"
+        >
+          Open Dashboard
         </Link>
       </div>
     </div>
